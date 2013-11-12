6cd0dd0a6f296644201399ef4a09291d6d757172
Marco Aurelio Lotz <lotzutfpr@gmail.com>
2013-11-12 01:09:15 -0200

    license and README modifications

--- polarScript.m
+++ polarScript.m
@@ -1,25 +1,40 @@
-%This script was created by Marco Aurelio Gomes Lotz and is available under
-%Apache License. For more details, visit: http://www.apache.org/licenses/
-%Originally Available from: github.com/marcolotz
+%  Copyright 2013 Marco Aurelio Barbosa Fagnani Gomes Lotz
+% 
+%    Licensed under the Apache License, Version 2.0 (the "License"); you
+%    may not use this file except in compliance with the License. You may
+%    obtain a copy of the License at
+% 
+%        http://www.apache.org/licenses/LICENSE-2.0
+% 
+%    Unless required by applicable law or agreed to in writing, software
+%    distributed under the License is distributed on an "AS IS" BASIS,
+%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+%    implied. See the License for the specific language governing
+%    permissions and limitations under the License.
+%==========================================================================
 
-%Function Description:
-%Now, ph(6:17) are handles of text() objects whose String property is an 
-%angle. The correspondences are:
-%6: 0, 7: 180, 8: 330, 9: 150, 10: 300, 11: 120, 12: 270, 13: 90, 14: 240,
-%15: 60, 16: 210, 17: 30
-%Thus you would, for example, set(ph(8),'String','-30')
-%set(ph(6),'String','0 seconds');
-%set(ph(7),'String',num2str(size(audioData,1)/2/Fs));
-%set(ph(8),'String',num2str(size(audioData,1)*330/(360*Fs)));
-%set(ph(9),'String',num2str(size(audioData,1)*150/(360*Fs)));
-%set(ph(10),'String',num2str(size(audioData,1)*300/(360*Fs)));
-%set(ph(11),'String',num2str(size(audioData,1)*120/(360*Fs)));
-%set(ph(12),'String',num2str(size(audioData,1)*270/(360*Fs)));
-%set(ph(13),'String',num2str(size(audioData,1)*90/(360*Fs)));
-%set(ph(14),'String',num2str(size(audioData,1)*240/(360*Fs)));
-%set(ph(15),'String',num2str(size(audioData,1)*60/(360*Fs)));
-%set(ph(16),'String',num2str(size(audioData,1)*210/(360*Fs)));
-%set(ph(17),'String',num2str(size(audioData,1)*30/(360*Fs)));
+% This script was created by Marco Aurelio Gomes Lotz and is available under
+% Apache License. For more details, visit: http://www.apache.org/licenses/
+% Originally Available from: github.com/marcolotz
+
+% Function Description:
+% Now, ph(6:17) are handles of text() objects whose String property is an 
+% angle. The correspondences are:
+% 6: 0, 7: 180, 8: 330, 9: 150, 10: 300, 11: 120, 12: 270, 13: 90, 14: 240,
+% 15: 60, 16: 210, 17: 30
+% Thus you would, for example, set(ph(8),'String','-30')
+% set(ph(6),'String','0 seconds');
+% set(ph(7),'String',num2str(size(audioData,1)/2/Fs));
+% set(ph(8),'String',num2str(size(audioData,1)*330/(360*Fs)));
+% set(ph(9),'String',num2str(size(audioData,1)*150/(360*Fs)));
+% set(ph(10),'String',num2str(size(audioData,1)*300/(360*Fs)));
+% set(ph(11),'String',num2str(size(audioData,1)*120/(360*Fs)));
+% set(ph(12),'String',num2str(size(audioData,1)*270/(360*Fs)));
+% set(ph(13),'String',num2str(size(audioData,1)*90/(360*Fs)));
+% set(ph(14),'String',num2str(size(audioData,1)*240/(360*Fs)));
+% set(ph(15),'String',num2str(size(audioData,1)*60/(360*Fs)));
+% set(ph(16),'String',num2str(size(audioData,1)*210/(360*Fs)));
+% set(ph(17),'String',num2str(size(audioData,1)*30/(360*Fs)));
 
 
 %input: Number of samples - the number of samples in the evaluated song.
